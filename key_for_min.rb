eda33f8d05e91edcd4087cac417e87ed78b20fb8
Justin Movitz <movitzj1@montclair.edu>
2021-01-21 13:23:58 -0500

    commit tree a688ceb6dbaa283a78a6e4a1fd645ca73211d375

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,33 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+#name_hash = {"1" => 10, "2" => 50, "3" => 17} 
+#name_hash = {1 => 10, 2 => 50, 3 => 17}
+#name_hash = {:blake => 10, :ashley => 50, :adam => 17}
+#name_hash = {:blake => "Star", :ashley => "wars", :adam => "blue"}
 
+def key_for_min_value(name_hash)
+    min_key = " "
+    min_value = nil
+    #min_key = nil 
+        #(also works)
+    #min_value = " " 
+        #(I belive this works too)
+    if name_hash.empty?
+        return nil
+    end
+    name_hash.each do |key, value|
+        #if min_value == " "
+            #(I belive this works if implemented properly)
+        if min_value == nil     
+            min_value = value
+            min_key = key
+        elsif value < min_value
+            min_value = value
+            min_key = key
+        end
+    end
+    min_key
 end
+
+#puts key_for_min_value(name_hash)
